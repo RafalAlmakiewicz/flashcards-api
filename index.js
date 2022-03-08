fda12982bfe7dc2014d619bdad7c9de373b6955f
Rafal Almakiewicz <ralmakiewicz@gmail.com>
2022-03-08 17:35:48 +0100

    Deployment preparation.

--- index.js
+++ index.js
@@ -4,19 +4,17 @@ const decks = require("./routes/decks");
 const seed = require("./routes/seed");
 const mongoose = require("mongoose");
 const Joi = require("joi");
-//Joi.objectId = require("joi-objectid")(Joi);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/flashcards/api/decks", decks);
 app.use("/flashcards/api/seed", seed);
-//app.use(express.static("public"));
 
 mongoose
-  .connect("mongodb://localhost/flashcards")
-  .then(() => console.log("connected to mongodb..."))
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("connected to mongodb"))
   .catch((err) => console.error(err));
 
 const port = process.env.PORT || 3000;
-app.listen(3000, () => console.log(`flashcards-api on port ${port}...`));
+app.listen(port, () => console.log(`flashcards-api on port ${port}...`));
